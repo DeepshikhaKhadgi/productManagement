ac0c7af3e694a0e73c087c0ffe142f5ddc99f556
DeepshikhaKhadgi <deepshikhakhadgi@gmail.com>
2017-06-16 02:05:59 -0500

    adding files

---
+++ app/products/productDetailCtrl.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("productManagement")
+    .controller("ProductDetailCtrl", ["productResource", "$stateParams", "productService","product", ProductDetailCtrl]);
+
+    function ProductDetailCtrl(productResource, $stateParams, productService, product) {
+        var vm = this;
+       
+       vm.product = product;
+      //  vm.product = productResource.get({ productId: $stateParams.productId });
+
+        
+        vm.title = "Product Detail: " + vm.product.productName;
+
+        if (vm.product.tags) {
+            vm.product.tagList = vm.product.tags.toString();
+        }
+
+        vm.marginPercent = productService.calculateMarginPercent(vm.product.price, vm.product.cost);
+    }
+}())
